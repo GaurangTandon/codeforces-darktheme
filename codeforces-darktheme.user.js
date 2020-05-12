d10a2461639760fa34be2e6ddae629a68a9d7a5c
Gaurang Tandon <1gaurangtandon@gmail.com>
2020-05-12 13:12:48 +0530

    bump version

--- codeforces-darktheme.user.js
+++ codeforces-darktheme.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Codeforces dark theme
-// @version      1.0.5
+// @version      1.0.6
 // @description  dark theme for codeforces
 // @author       Gaurang Tandon
 // @match        https://codeforces.com/*
