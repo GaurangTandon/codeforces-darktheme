6841c8efe7c6809bcba7717be931c974f52d424b
Gaurang Tandon <gaurang.tandon@students.iiit.ac.in>
2021-04-08 18:47:47 +0530

    Fix weird white background in transparent images

--- codeforces-darktheme.user.js
+++ codeforces-darktheme.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Codeforces dark theme
-// @version      1.0.7
+// @version      1.0.8
 // @description  dark theme for codeforces
 // @author       Gaurang Tandon
 // @match        https://codeforces.com/*
