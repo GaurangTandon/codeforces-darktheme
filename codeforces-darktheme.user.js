eef5722d66f91dff10800b1b66e603c2175b6519
LordLava <davidstarup@gmail.com>
2021-08-10 13:17:47 +0300

    Changed first letter

--- codeforces-darktheme.user.js
+++ codeforces-darktheme.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Codeforces dark mode
-// @version      1.0.9
+// @version      1.1.0
 // @description  Improved dark mode for Codeforces
 // @author       Gaurang Tandon & fork by David
 // @match        https://codeforces.com/*
