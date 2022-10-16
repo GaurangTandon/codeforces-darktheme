8cbea494c1f74e474ca83f534d99893a5addb8f0
Gaurang Tandon <1gaurangtandon@gmail.com>
2022-10-16 11:06:43 +0530

    bump version
    fixes #49

--- codeforces-darktheme.user.js
+++ codeforces-darktheme.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Codeforces dark theme
-// @version      1.1.0
+// @version      1.1.1
 // @description  dark theme for codeforces
 // @author       Gaurang Tandon
 // @match        https://codeforces.com/*
